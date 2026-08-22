1ffc8f0d3fa937fb51520d715df16663b550ae6a
valetical <87675974+valetical@users.noreply.github.com>
2026-08-22 02:28:08 -0700

    Add files via upload

---
+++ scripts/releaseslynu.js
@@ -0,0 +1,548 @@
+const artistlinks = {
+  lynU: "https://soundcloud.com/lynyou",
+  yuzu: "https://soundcloud.com/yuzuberri",
+  rel: "https://soundcloud.com/2rel",
+  crusafix: "https://soundcloud.com/1crusafix",
+  akkiro: "https://soundcloud.com/7akkiro",
+  isai: "https://soundcloud.com/omgisai",
+  deepinparis: "https://soundcloud.com/deepinparis",
+  rhekdens: "https://soundcloud.com/rhekdens",
+  "Alyx Trick": "https://alyxtrick.com",
+  "ellie mills": "https://soundcloud.com/elliemillss",
+  xaev: "https://unironical.ly",
+  "DECO*27": "https://www.youtube.com/channel/UCGmO0S4S-AunjRdmxA6TQYg",
+  "Toby Fox": "https://x.com/tobyfox",
+  Camellia: "https://cametek.jp",
+  urchin_ltd: "https://x.com/urchin_ltd",
+  inci0: "https://hachinemu.com",
+  "#3six0": "https://soundcloud.com/3six03six03six0",
+  "Jonathan Coulton": "https://www.jonathancoulton.com"
+};
+
+const releases = [
+  {
+    title: "Flower Man",
+    cover: "https://i1.sndcdn.com/artworks-NhvdM6zjJRazQkbm-Pht19w-t500x500.jpg",
+    date: "8/14/26",
+    type: "Single",
+    runtime: "3:21",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: [
+      "Toby Fox - composer",
+      "Camellia - composer",
+      "lynU - arranger",
+      "yuzu - arranger",
+      "Hayden (Synthesizer V) - vocals",
+      "urchin_ltd - artwork"
+    ]
+  },
+  {
+    title: "Endless8",
+    cover: "https://i1.sndcdn.com/artworks-7ArllCCkogme33YY-O8YfyA-t500x500.jpg",
+    date: "8/6/26",
+    type: "Single",
+    runtime: "2:15",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals", "rel - production"]
+  },
+
+  {
+    title: "Tell me",
+    cover: "https://i1.sndcdn.com/artworks-Mz5Hg0zC7HaAgEgO-R46yzA-t500x500.jpg",
+    date: "5/1/26",
+    type: "Single",
+    runtime: "1:44",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "Want You Gone [cover]",
+    cover: "https://i1.sndcdn.com/artworks-GXrlUU4hJya9C9cq-0WGPPA-t500x500.jpg",
+    date: "4/11/26",
+    type: "Single",
+    runtime: "2:18",
+    platforms: ["sc"],
+    rating: 2,
+    credits: ["Jonathan Coulton - composer", "lynU - production, vocals"]
+  },
+
+  {
+    title: "Sacrifice",
+    cover: "https://i1.sndcdn.com/artworks-RLTqmMHyaUNkWnmL-xI9Wzg-t500x500.jpg",
+    date: "3/13/26",
+    type: "Single",
+    runtime: "5:31",
+    platforms: ["sc", "spt", "apl", "yt", "bc"],
+    rating: 3,
+    credits: ["lynU - production, vocals"],
+    tracks: [
+      ["Sacrifice", "3:31"],
+      ["Take a chance", "2:00"]
+    ]
+  },
+
+  {
+    title: "sideFX",
+    cover: "https://i1.sndcdn.com/artworks-ovJx0plNDdGK5eUF-NPCwEA-t500x500.jpg",
+    date: "12/26/25",
+    type: "Single",
+    runtime: "2:13",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "monitoring 【lynU bootleg】",
+    cover: "https://i1.sndcdn.com/artworks-oGPy857lXxOx4eBV-MyGc1w-original.png",
+    date: "12/26/25",
+    type: "Single",
+    runtime: "2:13",
+    platforms: ["sc"],
+    rating: 3,
+    credits: ["DECO*27 - production", "Hatsune Miku - vocals", "lynU - production, remixer"]
+  },
+
+  {
+    title: "pay attention",
+    cover: "https://i1.sndcdn.com/artworks-cgFlnGbqw60oageu-EjiKVg-t500x500.jpg",
+    date: "9/19/25",
+    type: "Single",
+    runtime: "3:35",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals", "Alyx Trick - vocals"]
+  },
+
+  {
+    title: "stuck in...Yesterday_3",
+    cover: "https://i1.sndcdn.com/artworks-bsUxoNyHGglvbKey-qsbMMQ-t500x500.jpg",
+    date: "7/25/25",
+    type: "Single",
+    runtime: "3:03",
+    platforms: ["sc"],
+    rating: 1,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "Mask-off",
+    cover: "https://i1.sndcdn.com/artworks-EapPc5jTT8SPrfSb-JfMZUg-t500x500.jpg",
+    date: "7/5/25",
+    type: "Single",
+    runtime: "1:56",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "Raise Up Your Bat [Arrangement]",
+    cover: "https://i1.sndcdn.com/artworks-wEzv6VI4gSxNzo8T-ijbblQ-t500x500.jpg",
+    date: "6/28/25",
+    type: "Single",
+    runtime: "2:06",
+    platforms: ["sc"],
+    rating: 0,
+    credits: ["Toby Fox - composer", "lynU - production, vocals"]
+  },
+
+  {
+    title: "soul",
+    cover: "https://i1.sndcdn.com/artworks-alhBRkG23ycPkjMg-v0eHzQ-t500x500.jpg",
+    date: "6/11/25",
+    type: "Single",
+    runtime: "2:48",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: ["lynU - production, vocals", "crusafix - vocals", "akkiro - vocals"]
+  },
+
+  {
+    title: "deathwish -fun mix-",
+    cover: "https://i1.sndcdn.com/artworks-Y6J4VNUVxXJ1IQNa-LJm9fw-t500x500.jpg",
+    date: "5/30/25",
+    type: "Single",
+    runtime: "2:49",
+    platforms: ["sc"],
+    rating: 1,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "#tbt",
+    cover: "https://i1.sndcdn.com/artworks-fsJmqHwZKvHaZzBz-2BlpcQ-t500x500.png",
+    date: "5/24/25",
+    type: "EP",
+    runtime: "7:55",
+    platforms: ["sc", "spt", "apl", "yt", "bc"],
+    rating: 1,
+    credits: [
+      "lynU - production, vocals",
+      "isai - vocals (track 1)",
+      "Alyx Trick - vocals (track 2)",
+      "crusafix - vocals (track 3)",
+      "rhekdens - production (track 3)",
+      "miichware - artwork"
+    ],
+    tracks: [
+      ["dimension", "1:52"],
+      ["surprise", "1:59"],
+      ["heartBeat", "1:59"],
+      ["fakeFriends", "2:03"]
+    ]
+  },
+
+  {
+    title: "bandAid✚",
+    cover: "https://i1.sndcdn.com/artworks-4l121EyGJBjvQoN3-dCIizw-t500x500.jpg",
+    date: "5/12/25",
+    type: "Single",
+    runtime: "1:42",
+    platforms: ["sc"],
+    rating: 0,
+    credits: ["lynU - vocals", "isai - production"]
+  },
+
+  {
+    title: "Test.Ur.Luck",
+    cover: "https://i1.sndcdn.com/artworks-yW7Tec0iyf0Nr4al-CpNySQ-t500x500.jpg",
+    date: "4/20/25",
+    type: "Single",
+    runtime: "2:22",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "story // voyage 1969",
+    cover: "https://i1.sndcdn.com/artworks-WRVWyM4yRXFMzQsG-5GoEzA-t500x500.jpg",
+    date: "4/11/25",
+    type: "Single",
+    runtime: "2:08",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 2,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "catchUp",
+    cover: "https://i1.sndcdn.com/artworks-3epo63cj78HXCqnV-8ITOpg-t500x500.jpg",
+    date: "3/26/25",
+    type: "Single",
+    runtime: "1:54",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: ["lynU - vocals", "Alyx Trick - vocals", "xaev - production"]
+  },
+
+  {
+    title: "10",
+    cover: "https://i1.sndcdn.com/artworks-gD6P62tYylAsyhcy-AGyOfw-t500x500.jpg",
+    date: "3/10/25",
+    type: "Single",
+    runtime: "1:48",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: [
+      "#3six0 - collective",
+      "lynU - production, vocals",
+      "Alyx Trick - vocals, artwork",
+      "deepinparis - vocals"
+    ]
+  },
+
+  {
+    title: "lynU @ channel '25",
+    cover: "https://i1.sndcdn.com/artworks-Cbo7IWry5BOnSXqA-QJy8pg-t500x500.jpg",
+    date: "2/22/25",
+    type: "Mix",
+    runtime: "17:00",
+    platforms: ["sc", "yt"],
+    rating: 0,
+    credits: ["lynU - production, visuals"]
+  },
+
+  {
+    title: "deathwish",
+    cover: "https://i1.sndcdn.com/artworks-QFJ6B76pHRLLt1gu-yYcAdQ-original.jpg",
+    date: "1/31/25",
+    type: "Single",
+    runtime: "2:32",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "flash",
+    cover: "https://i1.sndcdn.com/artworks-sZV3dW5z2KzIyHGL-LuCrqg-t500x500.jpg",
+    date: "1/6/25",
+    type: "Single",
+    runtime: "1:40",
+    platforms: ["sc"],
+    rating: 0,
+    credits: ["lynU - production, vocals", "isai - production, vocals"]
+  },
+
+  {
+    title: "forever",
+    cover: "https://i1.sndcdn.com/artworks-aKVt972ZpPfQlvyz-dmLaZw-t500x500.png",
+    date: "11/30/24",
+    type: "Single",
+    runtime: "1:42",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: ["#3six0 - collective", "lynU - production, vocals", "crusafix - vocals"]
+  },
+
+  {
+    title: "potions",
+    cover: "https://i1.sndcdn.com/artworks-fnR4dRohCpUKKOTy-XbYkzQ-t1080x1080.jpg",
+    date: "11/29/24",
+    type: "Single",
+    runtime: "2:29",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "honest",
+    cover: "https://i1.sndcdn.com/artworks-XJo8y4YwRfmpvTZf-ZN9OrQ-t500x500.png",
+    date: "11/24/24",
+    type: "Single",
+    runtime: "1:46",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 1,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "focus",
+    cover: "https://i1.sndcdn.com/artworks-3b1zOj0ZKEKWiEK0-fwyg4Q-t500x500.jpg",
+    date: "11/10/24",
+    type: "Single",
+    runtime: "1:54",
+    platforms: ["sc"],
+    rating: 1,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "r u ready",
+    cover: "https://i1.sndcdn.com/artworks-IyUB512C74Q9zFE0-rGqPpg-t500x500.jpg",
+    date: "10/31/24",
+    type: "Single",
+    runtime: "1:39",
+    platforms: ["sc"],
+    rating: 2,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "Star",
+    cover: "https://i1.sndcdn.com/artworks-iZGvUZnJnWzsuZyx-2nkHdQ-t500x500.jpg",
+    date: "10/18/24",
+    type: "Single",
+    runtime: "1:46",
+    platforms: ["sc"],
+    rating: 0,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "u & i",
+    cover: "https://i1.sndcdn.com/artworks-VOjWPv0S4EGBRzSN-K2ekAA-t1080x1080.jpg",
+    date: "9/20/24",
+    type: "Single",
+    runtime: "3:16",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "innovation",
+    cover: "https://i1.sndcdn.com/artworks-ZKLMEdyGjdVaPzGv-VE98ow-t500x500.jpg",
+    date: "9/16/24",
+    type: "Single",
+    runtime: "2:21",
+    platforms: ["sc"],
+    rating: 0,
+    credits: [
+      "lynU - production, vocals",
+      "deepinparis - production, vocals",
+      "crusey - production",
+      "crusafix - vocals",
+      "isai - vocals"
+    ]
+  },
+
+  {
+    title: "idle",
+    cover: "https://i1.sndcdn.com/artworks-NGtDEjqselEL6x61-nga77A-t1080x1080.jpg",
+    date: "8/30/24",
+    type: "Single",
+    runtime: "2:56",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals", "isai - production"]
+  },
+
+  {
+    title: "trace",
+    cover: "https://i1.sndcdn.com/artworks-2upczD1baDsVyDEx-lhgBJw-t1080x1080.jpg",
+    date: "7/19/24",
+    type: "Single",
+    runtime: "2:30",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals", "inci0 - production, vocals"]
+  },
+
+  {
+    title: "too much",
+    cover: "https://i1.sndcdn.com/artworks-Qjq8Ye2nSCKLrGpT-7i8LpA-t500x500.jpg",
+    date: "6/21/24",
+    type: "Single",
+    runtime: "2:12",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: [
+      "#3six0 - collective",
+      "lynU - production, vocals",
+      "Alyx Trick - vocals",
+      "crusafix - vocals",
+      "isai - production"
+    ]
+  },
+
+  {
+    title: "Burning bridges",
+    cover: "https://i1.sndcdn.com/artworks-kyuyhElIn29T6BLj-rXPAGQ-t500x500.jpg",
+    date: "7/4/24",
+    type: "Single",
+    runtime: "2:09",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "Go ahead",
+    cover: "https://i1.sndcdn.com/artworks-Bbdj96O6UrfRHC6Y-nxgTlA-t500x500.jpg",
+    date: "7/1/24",
+    type: "Single",
+    runtime: "1:28",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 2,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "lifeline",
+    cover: "https://i1.sndcdn.com/artworks-CgN1Na8AZyOca39O-Z8QDxQ-t500x500.jpg",
+    date: "6/21/24",
+    type: "Single",
+    runtime: "2:03",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["#3six0 - collective", "lynU - production, vocals", "isai - vocals"]
+  },
+
+  {
+    title: "you again",
+    cover: "https://i1.sndcdn.com/artworks-l1h03bSNX9hTInyD-vHj1eg-t1080x1080.jpg",
+    date: "5/31/24",
+    type: "Single",
+    runtime: "2:19",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 1,
+    credits: ["lynU - production, vocals", "isai - vocals"]
+  },
+
+  {
+    title: "post-dream",
+    cover: "https://i1.sndcdn.com/artworks-Iz6pbL0YPBUG3M5g-4yXIqQ-t1080x1080.jpg",
+    date: "4/26/24",
+    type: "EP",
+    runtime: "12:00",
+    platforms: ["sc", "spt", "apl", "yt", "bc"],
+    rating: 2,
+    credits: ["lynU - production, vocals"],
+    tracks: [
+      ["love liar", "2:33"],
+      ["melancholy", "2:14"],
+      ["class president", "3:20"],
+      ["framework", "1:56"],
+      ["the art of losing", "1:56"]
+    ]
+  },
+
+  {
+    title: "reverie",
+    cover: "https://i1.sndcdn.com/artworks-1KHAkYHMY4hJ8S2G-cR0X3Q-t1080x1080.jpg",
+    date: "3/12/24",
+    type: "Album",
+    runtime: "37:12",
+    platforms: ["sc", "spt", "apl", "yt", "bc"],
+    rating: 2,
+    credits: [
+      "lynU - production, vocals",
+      "Alyx Trick - production, vocals",
+      "akkiro - vocals",
+      "crusafix - additional vocals",
+      "ellie mills - co-writer"
+    ],
+    tracks: [
+      ["war within", "2:19"],
+      ["bad actor", "2:48"],
+      ["dreamer", "4:13"],
+      ["knock me dead", "4:13"],
+      ["packet loss", "4:20"],
+      ["haunt", "4:05"],
+      ["debris", "6:00"],
+      ["replace", "3:09"],
+      ["what's wrong - CD bonus", "6:05"]
+    ]
+  },
+
+  {
+    title: "lynU @ channel '24",
+    cover: "https://i1.sndcdn.com/artworks-dfHgsfw8YGcSO99u-ulYPQg-t500x500.jpg",
+    date: "2/18/24",
+    type: "Mix",
+    runtime: "15:00",
+    platforms: ["sc", "yt"],
+    rating: 0,
+    credits: ["lynU - production"]
+  },
+
+  {
+    title: "hurtz",
+    cover: "https://i1.sndcdn.com/artworks-eR6szFTifKsumzk7-3NzmXQ-t500x500.jpg",
+    date: "10/18/23",
+    type: "Single",
+    runtime: "2:08",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: ["lynU - production, vocals", "isai - production"]
+  },
+
+  {
+    title: "need me",
+    cover: "https://i1.sndcdn.com/artworks-mUDx2UcrYevPdVe3-tMT1ag-t500x500.jpg",
+    date: "10/11/23",
+    type: "Single",
+    runtime: "2:34",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals, visuals", "Alyx Trick - production, vocals", "isai - production"]
+  }
+];
---
+++ scripts/releasesrld.js
@@ -0,0 +1,226 @@
+const artistlinks = {
+  lynU: "https://soundcloud.com/lynyou",
+  "ellie mills": "https://soundcloud.com/elliemillss"
+};
+
+const releases = [
+/*
+  {
+    title: "215",
+    cover: "https://i1.sndcdn.com/artworks-FNnCVzWzS7JgxrAe-uUQzSg-t500x500.jpg",
+    date: "?",
+    type: "Single",
+    runtime: "?",
+    rating: 0,
+    credits: ["lynU - production, vocals"]
+  },
+*/
+
+  {
+    title: "Part of town",
+    cover: "https://i1.sndcdn.com/artworks-FNnCVzWzS7JgxrAe-uUQzSg-t500x500.jpg",
+    date: "7/11/26",
+    type: "Single",
+    runtime: "1:53",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "Adore me",
+    cover: "https://i1.sndcdn.com/artworks-FNnCVzWzS7JgxrAe-uUQzSg-t500x500.jpg",
+    date: "5/12/26",
+    type: "Single",
+    runtime: "1:44",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "Porchlight",
+    cover: "https://i1.sndcdn.com/artworks-CS58zrzQiJNfo0pM-Rp36cQ-t500x500.jpg",
+    date: "1/9/26",
+    type: "Single",
+    runtime: "2:25",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "Turning point",
+    cover: "https://i1.sndcdn.com/artworks-syd5qehbcuwyQFx8-QP7MvQ-t500x500.jpg",
+    date: "11/14/25",
+    type: "Single",
+    runtime: "2:26",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals"]
+  },
+  
+                {
+    title: "even if it kills me",
+    cover: "https://i1.sndcdn.com/artworks-9Hc7S5xJDo4yOLNy-MapNxQ-t500x500.jpg",
+    date: "10/21/25",
+    type: "Single",
+    runtime: "2:06",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: ["lynU - production, vocals"],
+  },
+
+  {
+    title: "Like the last time",
+    cover: "https://i1.sndcdn.com/artworks-QyznUlvEwtAegOEP-beBsVg-t500x500.jpg",
+    date: "7/15/25",
+    type: "Single",
+    runtime: "2:04",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 1,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "Codependent",
+    cover: "https://i1.sndcdn.com/artworks-E8j4ZEIYPYqDktHB-qJ6eAg-t500x500.jpg",
+    date: "6/19/25",
+    type: "Single",
+    runtime: "3:00",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 2,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "Running in circles",
+    cover: "https://i1.sndcdn.com/artworks-wprioGsXPMgxcaNY-6XtxSQ-t500x500.jpg",
+    date: "4/25/25",
+    type: "Single",
+    runtime: "1:34",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: ["lynU - production, vocals"]
+  },
+
+  {
+    title: "Drafts",
+    cover: "https://i1.sndcdn.com/artworks-6S6JjwqeeMSXhytM-jYdycQ-t1080x1080.jpg",
+    date: "2/14/25",
+    type: "EP",
+    runtime: "14:46",
+    platforms: ["sc", "spt", "apl", "yt", "bc"],
+    rating: 3,
+    credits: ["lynU - production, vocals", "ellie mills - additional vocals (track 2)"],
+    tracks: [
+      ["How to die standing still", "3:20"],
+      ["Broke my promise", "2:19"],
+      ["All the time", "3:20"],
+      ["Learning to love solitude", "3:05"],
+      ["Blunt", "2:40"]
+    ]
+  },
+
+  {
+    title: "I",
+    cover: "https://i1.sndcdn.com/artworks-WrfpVlY6k8PEEROR-jLijwA-t1080x1080.jpg",
+    date: "10/11/24",
+    type: "EP",
+    runtime: "7:52",
+    platforms: ["sc", "spt", "apl", "yt", "bc"],
+    rating: 1,
+    credits: ["lynU - production, vocals"],
+    tracks: [
+      ["Bother", "1:23"],
+      ["Its Cloudy In Here", "1:36"],
+      ["I Was Someone Else", "1:21"],
+      ["Daydream", "1:49"],
+      ["Arise", "1:41"]
+    ]
+  },
+  
+              {
+    title: "as far as i know",
+    cover: "https://i1.sndcdn.com/artworks-URQu0ZnF4ezVUkke-wUEl5w-t500x500.jpg",
+    date: "9/14/24",
+    type: "Single",
+    runtime: "1:32",
+    platforms: ["sc"],
+    rating: 0,
+    credits: ["lynU - production, vocals"],
+  },
+  
+            {
+    title: "bittersweet",
+    cover: "https://i1.sndcdn.com/artworks-1LCOoPzGYQApJ01x-znX87Q-t500x500.jpg",
+    date: "8/6/24",
+    type: "Single",
+    runtime: "1:19",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 3,
+    credits: ["lynU - production, vocals"],
+  },
+  
+          {
+    title: "crown lake",
+    cover: "https://i1.sndcdn.com/artworks-gybUTzU4gyoaJ0gE-9k6yAw-t500x500.jpg",
+    date: "5/18/24",
+    type: "Single",
+    runtime: "2:02",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: ["lynU - production, vocals"],
+  },
+  
+        {
+    title: "deep cuts",
+    cover: "https://i1.sndcdn.com/artworks-W3OF2QnE82kVuaGY-t9mzpw-t500x500.jpg",
+    date: "5/1/24",
+    type: "Single",
+    runtime: "1:57",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: ["lynU - production, vocals"],
+  },
+  
+      {
+    title: "where did u go",
+    cover: "https://i1.sndcdn.com/artworks-FUvuifC53KyhxJ49-iktb4w-t500x500.jpg",
+    date: "4/17/24",
+    type: "Single",
+    runtime: "1:53",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: ["lynU - production, vocals"],
+  },
+  
+    {
+    title: "watering dead plants",
+    cover: "https://i1.sndcdn.com/artworks-gZjGHzzvmpJhBggD-K5dnqw-t500x500.jpg",
+    date: "3/15/24",
+    type: "EP",
+    runtime: "6:28",
+    platforms: ["sc", "spt", "apl", "yt"],
+    rating: 0,
+    credits: ["lynU - production, vocals"],
+    tracks: [
+      ["parking lot", "1:27"],
+      ["midden", "1:31"],
+      ["love liar", "2:02"],
+      ["drive into a corner", "1:27"],
+    ]
+  },
+  
+        {
+    title: "my favorite lie",
+    cover: "https://i1.sndcdn.com/artworks-jw3uyYGzK5POVd8N-RlPRyQ-t500x500.jpg",
+    date: "2/6/24",
+    type: "Single",
+    runtime: "1:13",
+    platforms: ["sc"],
+    rating: 0,
+    credits: ["lynU - production, vocals"],
+  },
+  
+];
