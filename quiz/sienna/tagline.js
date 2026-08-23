f06c1264b97cbc043e877cc56267615e7d5727f8
valetical <87675974+valetical@users.noreply.github.com>
2026-08-23 00:43:54 -0700

    Delete quiz/sienna directory

--- quiz/sienna/tagline.js
+++
@@ -1,8 +0,0 @@
-fetch("thisissuchastupididea.json")
-    .then(function (r) {
-        return r.json();
-    })
-    .then(function (data) {
-        const messages = data.messages;
-        document.getElementById("welcome").textContent = messages[Math.floor(Math.random() * messages.length)];
-    });
